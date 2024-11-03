4dc18baa7e05dd37d011aa473d8a3c299c10fcf1
Aqil321821 <aqilrao321821@gmail.com>
2024-11-03 10:57:35 +0500

    navbar create

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,9 +1,13 @@
+import Navbar from "./components/Navbar"
+
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline text-red-700">
-    Hello world!
-  </h1>
+    <>
+    <Navbar />
+    </>
+  
+
   )
 }
 
---
+++ my-app/src/components/Navbar.tsx
@@ -0,0 +1,37 @@
+import logo from '../assets/logo.png';
+import lens from '../assets/lens.png';
+import arrow from '../assets/arrow.png';
+import search from "../assets/search.png"
+const Navbar = () => {
+  return (
+    <div className='flex p-4'>
+      <img src={logo} alt='logo' className='w-11 h-9 mt-1 ' />
+
+      <div className='flex border border-spacing-1 w-64 p-2 border-black ml-5'>
+        <img src={lens} alt='lens' className='w-6 h-5 mt-1' />
+        <input placeholder='Location'  />
+        <img src={arrow} alt='arrow' className='w-8 h-7  pr-2 ' />
+      </div>
+<div className='flex h-12 ml-4 border border-black'>
+    <input placeholder='Find Cars, Mobile phones and more...'  className='ml-3 w-96' />
+    <img src={search} alt="search" className='w-10 h-11.5 ' />
+</div>
+
+ <div className='flex h-12 p-3 ml-10 cursor-pointer'>
+    <h1 className='font-semibold'>ENGLISH</h1>
+    <img src={arrow} alt="arrow" className='w-8 h-6 ml-2' />
+ </div>
+ <div className='flex h-12 p-3 ml-5 cursor-pointer underline hover:no-underline'>
+    <h1 className='font-bold text-lg'>Login</h1>
+   
+ </div>
+ <div className='w-28 flex h-12 p-2 ml-5 cursor-pointer rounded-full border-yellow-500 '>
+    <h1 className='font-bold text-lg ml-3'>+ SELL</h1>
+   
+ </div>
+
+    </div>
+  );
+};
+
+export default Navbar;
